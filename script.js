0adffdc9ea803610b3b309dfdd028f7e360855bc
Ali-Samehh <alisameh71010@gmail.com>
2025-05-10 19:42:06 +0300

    adding click to play message

--- script.js
+++ script.js
@@ -3,26 +3,25 @@ const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
 const resultDisplay = document.getElementById("result");
 
-let playerScore = 0;
-let computerScore = 0;
-
+playerScore = 0;
+computerScore = 0;
 
 rockBtn.addEventListener('click', ()=> {
     let computerSelection = getComputerChoice()
-    playRound('rock', computerSelection);
+    playRound('rock', computerSelection);;
 })
 
-paperBtn.addEventListener('click', () => {
+paperBtn.addEventListener('click', ()=> {
     let computerSelection = getComputerChoice()
     playRound('paper', computerSelection);
 })
 
-scissorsBtn.addEventListener('click' , () => {
+scissorsBtn.addEventListener('click', ()=> {
     let computerSelection = getComputerChoice()
     playRound('scissors', computerSelection);
 })
 
-function resetGame() {
+function resetGame () {
     playerScore = 0;
     computerScore = 0;
 }
@@ -30,37 +29,35 @@ function resetGame() {
 function getComputerChoice () {
     const options = ['rock', 'paper', 'scissors'];
     const choice = options[Math.floor(Math.random() * options.length)];
-    return choice ;
+    return choice;
 }
+
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        resultDisplay.textContent = `It's a tie! You both chose ${playerSelection} / player: ${playerScore} - computer: ${computerScore}`;
+        resultDisplay.textContent =`It's a tie! You both chose ${playerSelection} / ${playerScore} - ${computerScore}`;
     }
-
     else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper') 
+        (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
         playerScore++;
-        resultDisplay.textContent = ` You win! ${playerSelection} beats ${computerSelection}/ player: ${playerScore} - computer: ${computerScore}`;  
+        resultDisplay.textContent =  `You win! ${playerSelection} beats ${computerSelection} / ${playerScore} - ${computerScore}`;
     }
 
     else {
         computerScore++;
-        resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}/ player: ${playerScore} - computer: ${computerScore}`;
+        resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection} / ${playerScore} - ${computerScore}`;
     }
 
     if (playerScore === 5) {
-        resultDisplay.textContent = `You win the game! Final score: player: ${playerScore} - computer: ${computerScore}`;
-        resetGame();
-    } else if (computerScore === 5) {
-        resultDisplay.textContent = `You lose the game! Final score: player: ${playerScore} - computer: ${computerScore}`;
+        resultDisplay.textContent = `You win the game! Final score: ${playerScore} - ${computerScore} \n Click to play again!`;
         resetGame();
     }
-
-
+    else if (computerScore === 5) {
+        resultDisplay.textContent = `You lose the game! Final score: ${playerScore} - ${computerScore} \n Click to play again!`;
+        resetGame();
+    }
 }
-
 
